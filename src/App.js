eead0ec5e4bbcc2ea8336f068c10469203e15fbf
Kartik Sharma <kartiknd999@gmail.com>
2024-05-08 17:03:51 +0530

    resolve error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Banner from './components/Banner'; 
